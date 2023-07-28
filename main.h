3e9d8fec6f8a0741e24159a3c35253f78fcb1282
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-07-28 11:06:40 +0300

    fix betty style

--- main.h
+++ main.h
@@ -5,7 +5,12 @@
 #include <stdio.h>
 #include <unistd.h>
 
-
+/**
+ * struct convert - Struct to store format conversion function pointers.
+ *
+ * @op: Format specifier (e.g., "%c", "%s", "%d").
+ * @fun: Pointer to the corresponding conversion function.
+ */
 typedef struct convert
 {
 	char *op;
@@ -19,10 +24,9 @@ int PrintChar(va_list list);
 int PrintString(va_list list);
 int PrintPrecentage(va_list list);
 int PrintInt(va_list list);
+int PrintBinary(va_list list);
+/* helper function */
 int _putchar(char c);
- /** Parses the input formatted string and processes format specifiers,
-  * invoking the corresponding conversion functions to print the    	  
-  * formatted output */ 
 int formatParser(const char *, ConvertSt *, va_list);
 int integer(int d, int shouldClear);
 #endif
